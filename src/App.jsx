d3d6871b608c5366049d230fdd7c0d520dda4ea9
Will Tengyuan Li <will19940430@gmail.com>
2022-05-25 22:26:36 -0600

    hidden scroll bar

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ export default function App() {
   return (
     <div className='app h-screen m-0'>
       <Header/>
-      <div className="section relative top-16 bg-blue-500 h-[calc(100vh-64px)] snap-mandatory snap-y overflow-scroll scroll-smooth">
+      <div className="section relative top-16 bg-blue-500 h-[calc(100vh-64px)] snap-mandatory snap-y overflow-scroll scroll-smooth scrollbar-hide">
         <Hero/>
         <Work/>
         <Cards/>
