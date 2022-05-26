f00092d323acba01730fab4fa0a06a00fb09b765
Will Tengyuan Li <will19940430@gmail.com>
2022-05-26 12:39:52 -0600

    added hero-section layout

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -2,6 +2,18 @@ import React from 'react'
 
 export default function hero() {
   return (
-    <div className='heroSection bg-gray-300 w-full h-[calc(100vh-64px)] snap-start' id='hero'>hero</div>
+    <div className='heroSection absolute bg-gray-300 w-full h-[calc(100vh-64px)] snap-start sm:flex flex-col md:grid grid-cols-3' id='hero'>
+      <div className="right bg-yellow-100  md:col-span-2">
+          <div className="hero-image-wrapper flex justify-center sm: md:relative top-32">
+            <img src="https://www.simplilearn.com/ice9/free_resources_article_thumb/tester-or-developer-what-suits-you-the-most.jpg" alt="" />
+          </div>
+      </div>
+      <div className="left text-center self-center md:col-span-1 ">
+        <h1 className='hero-title xs:text-2xl py-3 sm:text-3xl py-4 md:text-6xl pb-10'>Will Tengyuan Li</h1>
+        <h2 className="hero-subtitle xs:text-xl sm:text-2xl pb-3 md:text-3xl pb-10">Entry Level Web Developer</h2>
+        <p className="hero-description xs:text-md sm:text-lg pb-3 md:text-xl pb-10">Yesterday a mechanic. Today a developer. Love HTML, CSS, Javascript.</p>
+      </div>
+      
+    </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,14 @@ module.exports = {
     extend: {},
     fontFamily:{
       'Ubuntu':['Ubuntu']
+    },
+    screens: {
+      'xs': '240px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
     }
   },
   plugins: [
