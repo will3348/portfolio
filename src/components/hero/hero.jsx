088601ee6d1621267d552e994b6881bd8aa99374
Will Tengyuan Li <will19940430@gmail.com>
2022-05-26 13:04:45 -0600

    added layout in hero-section

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function hero() {
   return (
-    <div className='heroSection absolute bg-gray-300 w-full h-[calc(100vh-64px)] snap-start sm:flex flex-col md:grid grid-cols-3' id='hero'>
+    <div className='heroSection bg-gray-300 w-full h-[calc(100vh-64px)] snap-start sm:flex flex-col md:grid grid-cols-3' id='hero'>
       <div className="right bg-yellow-100  md:col-span-2">
           <div className="hero-image-wrapper flex justify-center sm: md:relative top-32">
             <img src="https://www.simplilearn.com/ice9/free_resources_article_thumb/tester-or-developer-what-suits-you-the-most.jpg" alt="" />
@@ -13,7 +13,6 @@ export default function hero() {
         <h2 className="hero-subtitle xs:text-xl sm:text-2xl pb-3 md:text-3xl pb-10">Entry Level Web Developer</h2>
         <p className="hero-description xs:text-md sm:text-lg pb-3 md:text-xl pb-10">Yesterday a mechanic. Today a developer. Love HTML, CSS, Javascript.</p>
       </div>
-      
     </div>
   )
 }
--- src/components/work/Work.jsx
+++ src/components/work/Work.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function work() {
   return (
-    <div className='bg-pink-100 w-full h-[calc(100vh-64px)] snap-start' id="work">work</div>
+    <div className='works bg-pink-100 w-full h-[calc(100vh-64px)] snap-start' id="work">work</div>
   )
 }
