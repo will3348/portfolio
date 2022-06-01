46170ba76430f6b7ac891f4ec2f07c34e3a0dd99
Will Tengyuan Li <will19940430@gmail.com>
2022-06-01 15:34:04 -0600

    added works page slider layout

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Navigation from '../navibar/Navigation'
+import logo from '../../logo_transparent.png'
 
 
 export default function header() {
   return (
     <div className='header bg-red-300  fixed top-0  w-full h-16 z-10 '>
-      <h1 className='title '>My Name Is Tengyuan Li</h1>
+      <div className='title '><img src="{logo}" alt="123" /></div>
       <Navigation/>
     </div>
   )
--- src/components/work/Work.jsx
+++ src/components/work/Work.jsx
@@ -2,6 +2,18 @@ import React from 'react'
 
 export default function work() {
   return (
-    <div className='works bg-pink-100 w-full h-[calc(100vh-64px)] snap-start' id="work">work</div>
+    <div className='works bg-pink-100 w-full h-[calc(100vh-64px)] snap-start flex justify-center ' id="work">
+      <div className="container flex justify-center">
+        <div className="slider bg-green-400 h-4/5 w-3/5 relative top-14 rounded-2xl flex justify-center overflow-hidden">
+          <div className="left h-full flex justify-center basis-1/4 bg-red-400">
+              left
+          </div>
+          <div className="right h-full flex  justify-center  basis-3/4 bg-yellow-100">
+              right 
+          </div>
+        </div>
+      </div>
+      
+    </div>
   )
 }
