5fc5ea59c23bba1b8a18ee2145516ad6ce5ae7bd
Will Tengyuan Li <will19940430@gmail.com>
2022-07-20 13:21:47 -0600

    disabled react-hook.exhaustive-deps

--- src/components/work/Work.js
+++ src/components/work/Work.js
@@ -8,12 +8,14 @@ import { useEffect } from 'react'
 
 export default function Work() {
   const [page, setPage] = useState(1);
+  
   const [current, setCurrent] = useState({
     currentImg: tic,
     currentTitle: 'Tic Tac Toe',
     currentDescription: 'Tic Tac Toe game. A web app build with Vue.js including 3 pages.',
     currentLink: 'https://melodious-klepon-61830f.netlify.app/'
   })
+  
   function currentPage() {
     if (page === 1) {
       setCurrent({
@@ -34,24 +36,21 @@ export default function Work() {
   useEffect(
 
     () => {
-
-
       currentPage()
-
-
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [page]
   )
   return (
     <div className='works bg-pink-100 w-full h-[calc(100vh-64px)] snap-start flex justify-center' id="work">
 
-      <div className="container w-4/5 bg-yellow-100 flex justify-center items-center gap-2">
+      <div className="container w-4/5 bg-yellow-100 flex justify-center items-center gap-2 overflow-hidden">
 
         <button onClick={() => { setPage(Math.max(1, page - 1)) }} type="button" className=" rotate-180 text-blue-700 border border-blue-700 hover:bg-blue-700 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:focus:ring-blue-800">
           <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
         </button>
 
         <div className='wrapper'>
-          <div className='w-full flex justify-center'>
+          <div className='w-full flex justify-center m-0'>
             <h3 className='mx-0 my-auto font-small leading-tight text-3xl mt-0 mb-2 text-blue-600'>Page {page}</h3>
           </div>
           <Workcard image={current.currentImg} title={current.currentTitle} description={current.currentDescription} link={current.currentLink} />
--- src/components/work/Workcard.js
+++ src/components/work/Workcard.js
@@ -8,7 +8,7 @@ export default function cards(props) {
     </a>
     <div className="md:p-5 xs:p-2">
       <a href={props.link}>
-        <h5 className=" font-bold tracking-tight text-gray-900 dark:text-white xl:mb-2 text-2xl md:text-lg xs:hidden mb-0 text-sm">{props.title}</h5>
+        <h5 className=" font-bold tracking-tight text-gray-900 dark:text-white xl:mb-2 text-2xl md:text-lg hidden xs:hidden mb-0 text-sm">{props.title}</h5>
       </a>
       <p className=" md:inline-block mb-3 font-normal text-gray-700 dark:text-gray-400  xs:hidden mb-0">{props.description}</p>
       <a href={props.link} target="_blank" rel="noreferrer noopener" className="items-center py-2 px-3 text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 lg:text-lg md:inline-flex text-md font-medium  xs:hidden text-xs ">
