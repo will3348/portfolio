63f2f37dcf14723f550286b02a6482feb56c278f
Will Tengyuan Li <will19940430@gmail.com>
2022-07-20 12:59:13 -0600

    fixed button in workpage

--- src/components/work/Work.js
+++ src/components/work/Work.js
@@ -44,20 +44,30 @@ export default function Work() {
   return (
     <div className='works bg-pink-100 w-full h-[calc(100vh-64px)] snap-start flex justify-center' id="work">
 
-      <div className="container w-4/5 bg-yellow-100 flex justify-center items-center">
-        <div className='wrapper'>
+      <div className="container w-4/5 bg-yellow-100 flex justify-center items-center gap-2">
 
+        <button onClick={() => { setPage(Math.max(1, page - 1)) }} type="button" className=" rotate-180 text-blue-700 border border-blue-700 hover:bg-blue-700 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:focus:ring-blue-800">
+          <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+        </button>
+
+        <div className='wrapper'>
+          <div className='w-full flex justify-center'>
+            <h3 className='mx-0 my-auto font-small leading-tight text-3xl mt-0 mb-2 text-blue-600'>Page {page}</h3>
+          </div>
           <Workcard image={current.currentImg} title={current.currentTitle} description={current.currentDescription} link={current.currentLink} />
 
-          <div className='w-full flex justify-center'>
-            <h1 className='mx-0 my-auto '>Page {page}</h1>
-          </div>
+
           <div className='w-full flex justify-around'>
-            <button className='bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow' onClick={() => { setPage(Math.max(1, page - 1)) }}>Previous</button>
-            <button className='bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow' onClick={() => { setPage(Math.max(2, page - 1)) }}>N e x t</button>
+            {/* <button className='bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow' onClick={() => { setPage(Math.max(1, page - 1)) }}>Previous</button> */}
+            {/* <button className='bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow' onClick={() => { setPage(Math.max(2, page - 1)) }}>N e x t</button> */}
           </div>
-          
+
         </div>
+
+        <button onClick={() => { setPage(Math.max(2, page - 1)) }} type="button" class="text-blue-700 border border-blue-700 hover:bg-blue-700 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:focus:ring-blue-800">
+          <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+        </button>
+
       </div>
 
 
